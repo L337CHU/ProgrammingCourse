2833f9119221fa5d33278455a2dd5b205f154917
L337CHU <38093293+L337CHU@users.noreply.github.com>
2019-01-14 21:43:52 -0800

    Add files via upload

---
+++ week3/Box.hpp
@@ -0,0 +1,29 @@
+/******************************************************************************
+ * * Author: Christopher Chu
+ * * Date: 01/21/2018
+ * * Description: This file holds the class declaration BOX
+******************************************************************************/
+
+#ifndef BOX_HPP
+#define BOX_HPP
+
+
+
+//Box class declaration
+class Box
+{
+	private:
+		double height;
+		double width;
+		double length;
+	public:
+		Box();
+		Box( double, double, double);
+		void setHeight();
+		void setWidth();
+		void setLength();
+		double calcVolume ();
+		double calcSurfaceArea();
+};
+#endif
+
